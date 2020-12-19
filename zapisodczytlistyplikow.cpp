b586d9687b3e0956f8825d9b1ec21f57e94fb1e1
Piotr Badełek <piotr.badelek@gmail.com>
2020-12-19 10:41:26 +0100

    Add files via upload

--- zapisodczytlistyplikow.cpp
+++ zapisodczytlistyplikow.cpp
@@ -1,9 +1,10 @@
 #include "zapisodczytlistyplikow.h"
 #include <string.h>
-
 #include <cstdlib>
 #include <stdio.h>
 #include <iostream>
+#include <fstream>
+
 
 using namespace std;
 
@@ -57,3 +58,32 @@ bool ZapisOdczytListyPlikow::odczytaj(ListaProduktow * lista, char nazwa[])
         return true;
     }
 }
+
+bool ZapisOdczytListyPlikow_test::odczytaj(ListaProduktow * lista, char nazwa[])
+{
+    string line;
+    ifstream myfile (nazwa);
+    if (myfile.is_open())
+    {
+        while ( getline (myfile,line) )
+        {
+            cout << line << '\n';
+        }
+        myfile.close();
+    }
+    else cout << "Unable to open file";
+}
+
+
+bool ZapisOdczytListyPlikow_test::zapisz(ListaProduktow * lista, char nazwa[])
+{
+    ofstream myfile ("example.txt");
+    if (myfile.is_open())
+    {
+        myfile << "This is a line.\n";
+        myfile << "This is another line.\n";
+        myfile.close();
+    }
+    else cout << "Unable to open file";
+}
+
